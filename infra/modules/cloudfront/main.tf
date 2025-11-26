e2d7b8cde675b60abadff33066ffc263b090231d
bcaglaraydin <berdancaglaraydin@gmail.com>
2025-11-26 21:24:19 +0100

    feat: add CloudFront distribution and Origin Access Control module

--- infra/modules/cloudfront/main.tf
+++ infra/modules/cloudfront/main.tf
@@ -35,6 +35,20 @@ resource "aws_cloudfront_distribution" "this" {
     max_ttl                = 86400
   }
 
+  custom_error_response {
+    error_code            = 403
+    response_code         = 200
+    response_page_path    = "/index.html"
+    error_caching_min_ttl = 10
+  }
+
+  custom_error_response {
+    error_code            = 404
+    response_code         = 200
+    response_page_path    = "/index.html"
+    error_caching_min_ttl = 10
+  }
+
   restrictions {
     geo_restriction {
       restriction_type = "none"
