7f54dbd33be3e5de23bc930e39cc741f6b796cd9
bcaglaraydin <berdancaglaraydin@gmail.com>
2026-01-17 23:26:37 +0100

    chore: add temp logo

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -18,6 +18,9 @@ const dmSans = DM_Sans({
 export const metadata: Metadata = {
   title: "Letterboxd Analysis",
   description: "Analyze your Letterboxd profile",
+  icons: {
+    icon: "/logo.svg",
+  },
 };
 
 export default function RootLayout({
